0fb99980b09a91b7debd15aa90d1b0114fab8fbe
tLok922 <102222561+tLok922@users.noreply.github.com>
2024-06-13 20:56:42 +0800

    Update contentScript.js
    Remove code that adds unnecessary labels, divs, and attributes (Only the input text box is required)

--- contentScript.js
+++ contentScript.js
@@ -9,23 +9,9 @@ chrome.storage.local.get(["crnList"], function(result) {
         crns.innerHTML = "";
 
         for(let i = 0; i < result.crnList.length; i++) {
-            const newDiv = document.createElement("div");
-
             const newInput = document.createElement("input");
-            newInput.setAttribute("id",`txt_crn${i+1}`);
-            newInput.setAttribute("maxlength",`5`);
-            newInput.setAttribute("type",`text`);
-            newInput.setAttribute("class","ui-textbox-input ui-widget-content");
             newInput.value = result.crnList[i];
-
-            const newLabel = document.createElement("label");
-            newLabel.setAttribute("for",`txt_crn${i+1}`);
-            newLabel.setAttribute("class","crn_label");
-            newLabel.textContent = "CRN";
-
-            newDiv.appendChild(newLabel);
-            newDiv.appendChild(newInput);
-            crns.appendChild(newDiv);
+            crns.appendChild(newInput);
         }
         addCRNbutton.click();
 
